df933b23d77a325cc3a627636208ba60e2a176fd
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-01-14 09:21:52 +0900

    create about page

---
+++ pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const About: NextPage = () => {
+  return (
+    <>
+      <h1>About</h1>
+    </>
+  );
+};
+
+export default About;
