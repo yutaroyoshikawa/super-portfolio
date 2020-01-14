3bbcd3f86136814d4315b79d78e48ce384c3ba3d
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-01-14 09:22:02 +0900

    create contact page

---
+++ pages/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <h1>Contact</h1>
+    </>
+  );
+};
+
+export default Contact;
